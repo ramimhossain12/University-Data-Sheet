cca95a8651ff80fe04cd72eea22a486ad9e53962
Ramim <ramimhridoy512@gmail.com>
2020-07-29 22:27:40 +0600

    first commit

---
+++ app/src/main/java/com/example/university_data_sheet/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.university_data_sheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
